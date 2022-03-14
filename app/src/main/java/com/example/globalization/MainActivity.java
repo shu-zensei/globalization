1bf52704d92f4f4921bd959c88a1e3d6df145ace
shu-zensei <shanqing3131@gmail.com>
2022-03-14 15:06:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/globalization/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.globalization;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        textView.setText("");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(R.string.Message);
+            }
+        });
+    }
+}
